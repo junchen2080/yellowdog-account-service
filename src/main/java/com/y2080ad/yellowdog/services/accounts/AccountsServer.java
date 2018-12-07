0ed16b7f25695c90e0943e89e35190cd2cf384e7
junchen2080 <40672248+junchen2080@users.noreply.github.com>
2018-12-07 16:24:32 -0500

    Make the code compile with registration server for now.

--- src/main/java/com/y2080ad/yellowdog/services/accounts/AccountsServer.java
+++ src/main/java/com/y2080ad/yellowdog/services/accounts/AccountsServer.java
@@ -1,7 +1,17 @@
 package com.y2080ad.yellowdog.services.accounts;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
+import com.y2080ad.yellowdog.services.accounts.config.AccountsConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@Import(AccountsConfiguration.class)
 public class AccountsServer {
 	/**
 	 * Run the application using Spring Boot and an embedded servlet engine.
@@ -10,8 +20,8 @@ public class AccountsServer {
 	 *            Program arguments - ignored.
 	 */
 	public static void main(String[] args) {
-		// Tell server to look for routes-server.properties or
-		// routes-server.yml
+		// Tell server to look for accounts-server.properties or
+		// accounts-server.yml
 		System.setProperty("spring.config.name", "accounts-server");
 
 		SpringApplication.run(AccountsServer.class, args);
